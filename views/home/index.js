3eec4ce5eb058bf33c65859575aa1674b8714101
hansaemsky <hslee_cs@woowahan.com>
2017-08-29 01:28:22 +0900

    흐음.어렵다.

--- views/home/index.js
+++ views/home/index.js
@@ -1,6 +1,8 @@
 import Woowahan from 'woowahan';
 import template from './index.hbs';
 
+var a = 'aa';
+
 export const HomeView = Woowahan.View.create('HomeView', {
   template
 });
--- views/posts/row-items/index.js
+++ views/posts/row-items/index.js
@@ -1,6 +1,7 @@
 import Woowahan from 'woowahan';
 import Template from './index.hbs';
 
+
 export default Woowahan.ItemView.create('RowItem', {
   template: Template,
   onSelectedRow(event, trigger) {
